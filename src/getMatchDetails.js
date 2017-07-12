5ce08c688cd3ae66f8d70c267075fd1bf9e94d58
koleok <kchamb3@gmail.com>
2017-07-12 00:36:46 -0400

    Well that didnt work, dropped babel detector

--- src/getMatchDetails.js
+++ src/getMatchDetails.js
@@ -1,10 +1,4 @@
-const reflector = require('js-function-reflector')
-
-const isBabel = !(class {}.toString().indexOf('class ') === 0)
-
-const functionReflector = isBabel
-  ? reflector.compiler('babel-preset-es2015')
-  : reflector
+const functionReflector = require('js-function-reflector')
 
 module.exports = func => {
   const reflectedFunction = functionReflector(func)
