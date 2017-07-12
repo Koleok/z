8c309266091492b4de6003d5ed7205db5f5d91a9
koleok <kchamb3@gmail.com>
2017-07-12 00:12:14 -0400

    Detect if running transpiled babel code

--- src/getMatchDetails.js
+++ src/getMatchDetails.js
@@ -1,10 +1,16 @@
-const functionReflector = require('js-function-reflector')
+const reflector = require('js-function-reflector')
+
+const isBabel = !(class {}.toString().indexOf('class ') === 0)
+
+const functionReflector = isBabel
+  ? reflector.compiler('babel-preset-es2015')
+  : reflector
 
 module.exports = func => {
   const reflectedFunction = functionReflector(func)
 
   return {
     args: reflectedFunction.args,
-    func
+    func,
   }
 }
